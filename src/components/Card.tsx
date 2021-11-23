37aa7929db748d762074b3bbdf30614c7df9838f
Chupriarti <wallx13@gmail.com>
2021-11-23 21:57:45 +0300

    add type for Card children

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 interface CardPros {
     width: string;
     height: string;
+    children?: React.ReactChild | React.ReactNode;
 }
 
-const Card = ({width, height}: CardPros) => {
+const Card = ({width, height, children}: CardPros) => {
   return (
     <div style={{width, height, backgroundColor: "silver"}}>
-      
+      {children}
     </div>
   );
 }
