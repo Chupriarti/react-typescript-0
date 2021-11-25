1877fc4db29c9c4c03b21548ae8c829683ad970f
Chupriarti <wallx13@gmail.com>
2021-11-25 20:14:29 +0300

    used axios to get users

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
@@ -5,6 +6,20 @@ import { IUser } from './types/types';
 
 function App() {
   const [users, setUsers] = React.useState<IUser[]>([])
+
+  React.useEffect(() => {
+    fetchUsers();
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
+      setUsers(response.data)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <Card 
