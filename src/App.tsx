f8b87a0c0af436b4f27e92e8992eb988f3ca2973
Chupriarti <wallx13@gmail.com>
2021-11-25 20:32:36 +0300

    prepared getting todos state

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,30 @@ import React from 'react';
 import Card, { CardVariant } from './components/Card';
 import List from './components/List';
 import UserItem from './components/UserItem';
-import { IUser } from './types/types';
+import { ITodo, IUser } from './types/types';
 
 function App() {
   const [users, setUsers] = React.useState<IUser[]>([])
+  const [todos, setTodos] = React.useState<ITodo[]>([])
 
   React.useEffect(() => {
     fetchUsers();
+    fetchTodos();
   }, [])
 
   async function fetchUsers() {
     try {
       const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
       setUsers(response.data)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      setTodos(response.data)
     } catch (e) {
       console.error(e);
     }
@@ -33,7 +44,8 @@ function App() {
       </Card>
       <List 
         items={users}
-        renderItem={(user: IUser) => <UserItem key={user.id} user={user} />} />
+        renderItem={(user: IUser) => <UserItem key={user.id} user={user} />} 
+      />
     </div>
   );
 }
