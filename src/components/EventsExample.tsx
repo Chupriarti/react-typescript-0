53a21403e2896ea74bd7cd7941b3e3d0e8ae274c
Chupriarti <wallx13@gmail.com>
2021-11-26 21:01:11 +0300

    prepared component for evemts

---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const EventsExample: FC = () => {
+    return (
+        <div>
+            <input type="text" />
+            <button></button>
+        </div>
+    )
+}
