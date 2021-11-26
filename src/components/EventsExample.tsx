d896b6b9b8c00171f82631951a6c3c01e83ab88f
Chupriarti <wallx13@gmail.com>
2021-11-26 21:03:18 +0300

    added controlled input state

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
 
 const EventsExample: FC = () => {
+    const [value, setValue] = React.useState<string>("");
     return (
         <div>
-            <input type="text" />
+            <input value={value} onChange={(e) => setValue(e.target.value)} type="text" />
             <button></button>
         </div>
     )
