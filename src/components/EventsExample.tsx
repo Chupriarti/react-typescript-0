0e251a1abac43c037ccfa3f1424bc9555b0d4555
Chupriarti <wallx13@gmail.com>
2021-11-26 21:09:28 +0300

    added typed change handler

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -2,9 +2,14 @@ import React, { FC } from 'react'
 
 const EventsExample: FC = () => {
     const [value, setValue] = React.useState<string>("");
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    }
+    
     return (
         <div>
-            <input value={value} onChange={(e) => setValue(e.target.value)} type="text" />
+            <input value={value} onChange={changeHandler} type="text" />
             <button></button>
         </div>
     )
