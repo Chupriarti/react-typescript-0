8d88781a45173745a89125f79bad13d770909f70
Chupriarti <wallx13@gmail.com>
2021-11-27 15:03:22 +0300

    added nav links

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import UsersPage from './components/UsersPage';
 import TodosPage from './components/TodosPage';
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
       <BrowserRouter>
         <div>
+          <div>
+            <NavLink to={"/users"}>Users</NavLink>
+            <NavLink to={"/todos"}>Todos</NavLink>
+          </div>
           <Routes>
             <Route path={"/users"} element={<UsersPage />}>
             </Route>
