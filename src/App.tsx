f167ea5b320a9c3de12d48f0c43561b2495e1e5b
Chupriarti <wallx13@gmail.com>
2021-11-27 15:01:20 +0300

    added pages users and todos to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import Card, { CardVariant } from './components/Card';
-import EventsExample from './components/EventsExample';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import UsersPage from './components/UsersPage';
+import TodosPage from './components/TodosPage';
 
 function App() {
   return (
-    <div>
-      <EventsExample />
-      <Card 
-        width="200px" 
-        height="300px"
-        variant = {CardVariant.outlined}
-        onClick={(num) => console.log("Hola!", num)}
-      >
-        <button>Button</button>
-      </Card>
-    </div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path={"/users"} element={<UsersPage />}>
+            </Route>
+            <Route path={"/todos"} element={<TodosPage />}>
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
