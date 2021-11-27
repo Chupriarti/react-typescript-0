26cc69582e06b4f6a11441f67fe357ec03d92fad
Chupriarti <wallx13@gmail.com>
2021-11-27 15:28:51 +0300

    added back button functionality

--- src/components/UserItemPage.tsx
+++ src/components/UserItemPage.tsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, {FC} from 'react';
 import { IUser } from '../types/types';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate } from 'react-router-dom';
 
 const UserItemPage: FC = () => {
     const [user, setUser] = React.useState<IUser | null>(null);
     const params = useParams();
+    const navigate = useNavigate ();
   
     React.useEffect(() => {
       fetchUsers();
@@ -22,7 +23,7 @@ const UserItemPage: FC = () => {
 
     return (
         <div>
-            <button>Back</button>
+            <button onClick={() => navigate("/users")}>Back</button>
             <h1>User's page {user?.name}</h1>
             <div>{user?.email}</div>
             <div>{user?.address?.city} {user?.address?.street} {user?.address?.zipcode}</div>
