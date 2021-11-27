9ec1805426f8876c985f25eb6e17ef017d4e3a52
Chupriarti <wallx13@gmail.com>
2021-11-27 15:10:41 +0300

    prepared pages for specific items with id

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import UsersPage from './components/UsersPage';
 import TodosPage from './components/TodosPage';
 import { NavLink } from 'react-router-dom';
+import UserItemPage from './components/UserItemPage';
+import TodoItemPage from './components/TodoItemPage';
 
 function App() {
   return (
@@ -13,10 +15,10 @@ function App() {
             <NavLink to={"/todos"}>Todos</NavLink>
           </div>
           <Routes>
-            <Route path={"/users"} element={<UsersPage />}>
-            </Route>
-            <Route path={"/todos"} element={<TodosPage />}>
-            </Route>
+            <Route path={"/users"} element={<UsersPage />}/>
+            <Route path={"/todos"} element={<TodosPage />}/>
+            <Route path={"/users/:id"} element={<UserItemPage />}/>
+            <Route path={"/todos/:id"} element={<TodoItemPage />}/>
           </Routes>
         </div>
       </BrowserRouter>
